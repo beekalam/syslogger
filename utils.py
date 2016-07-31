fabc18f481d6c2a6a8eeedd01bc78e8b54a459ea
Mohammad Reza Mansouri <beekalam@gmail.com>
2016-07-31 15:57:22 +0430

    [CODE] function parse_url for splitting urls

--- utils.py
+++ utils.py
@@ -4,15 +4,29 @@ from urlparse import urlparse
 def log(to_log=""):
 	syslog.syslog("[syslogger] " + to_log)
 
-def get_url_extension(url):
+def parse_url(url):
+	ret = dict()
 	url_parsed = urlparse(url)
-	PATH = 2
+	(SCHEME,NETLOC,PATH,PARAMS,QUERY,FRAGMENT) = (0,1,2,3,4,5)
+	scheme = url_parsed[SCHEME]
+	netloc = url_parsed[NETLOC]
+	params = url_parsed[PARAMS]
+	query = url_parsed[QUERY]
+	fragment = url_parsed[FRAGMENT]
 	path = url_parsed[PATH]
 	if path:
+		ret['path'] = path
 		last_section = path[path.rfind('/'):]
 		splitted = last_section.split('.')
 		if len(splitted) == 2:
-			return splitted[1]
+			ret['ext'] = splitted[1]
 
-	return ''
+	if netloc:
+		ret['netloc'] = netloc
+	if query:
+		ret['query'] = query
+	if params:
+		ret['params'] = params
+
+	return ret
 
